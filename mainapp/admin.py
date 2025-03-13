78d7b4cb780cffb31245d2cf3817b0556a3d6cd4
FairaIchsa <zelov02@yandex.ru>
2025-03-13 22:33:44 +0300

    new: admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from mainapp import models
+
+
+admin.site.register(models.Category, admin.ModelAdmin)
+admin.site.register(models.Product, admin.ModelAdmin)
+admin.site.register(models.Cart, admin.ModelAdmin)
