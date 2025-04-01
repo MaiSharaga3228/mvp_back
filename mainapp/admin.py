962644496a7335657eff41a5d89b9a73981abc3b
FairaIchsa <zelov02@yandex.ru>
2025-04-01 22:08:45 +0300

    upd: model views

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 from mainapp import models
 
 
-admin.site.register(models.Category, admin.ModelAdmin)
-admin.site.register(models.Product, admin.ModelAdmin)
-admin.site.register(models.Cart, admin.ModelAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(CategoryAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['slug'].required = False
+        return form
+
+
+class ProductAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(ProductAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['slug'].required = False
+        return form
+
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Product, ProductAdmin)
+# admin.site.register(models.Cart, admin.ModelAdmin)
